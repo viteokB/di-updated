fb2682782f85dee8d45ee169ec8e43529f2c812d
Braidenbikher Victor <viktor.brai@yandex.ru>
2024-12-17 18:41:31 +0500

    Протестировал FileReader, LowercaseWordHandler

---
+++ Tests/WordHandlersTests/LowercaseWordHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Tests.WordHandlersTests.TestDatas;
+using WordHandlers.Handlers;
+
+namespace Tests.WordHandlersTests
+{
+    [TestFixture]
+    public class LowercaseWordHandlerTests
+    {
+        [TestCaseSource(typeof(LowercaseTestData), nameof(LowercaseTestData.OneWordCases))]
+        public void LowercaseWordHandler_ApplyHandler_ShouldLowerOneWord(List<string> given, List<string> expected)
+        {
+            var actual = LowercaseWordHandler.ApplyWordHandler(given);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestCaseSource(typeof(LowercaseTestData), nameof(LowercaseTestData.FewWordsCases))]
+        public void LowercaseWordHandler_ApplyHandler_ShouldLowerFewWords(List<string> given, List<string> expected)
+        {
+            var actual = LowercaseWordHandler.ApplyWordHandler(given);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ Tests/WordReadersTests/FileReaderTests/FileReaderTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApprovalTests;
+using ApprovalTests.Core;
+using ApprovalTests.Reporters;
+using ApprovalTests.Writers;
+using FluentAssertions;
+using WordReaders.Readers;
+
+namespace Tests.WordReadersTests.FileReaderTests
+{
+    [TestFixture]
+    public class FileReaderTests
+    {
+        const string PARENT_DIR_PATH = @"WordReadersTests/FileReaderTests/FilesSample";
+
+        [UseReporter(typeof(DiffReporter))]
+        [Test]
+        public void FileReader_Read_ShouldReadNormalTxtCorrect()
+        {
+            var fileReader = new FileReader(@$"{PARENT_DIR_PATH}/txt1_correct.txt", Encoding.UTF8);
+
+            Approvals.Verify(CreateWordsOutput(fileReader.Read()));
+        }
+
+        [Test]
+        public void FileReader_Read_ShouldThrowExceptionOnWrongTxt()
+        {
+            var fileReader = new FileReader(@$"{PARENT_DIR_PATH}/txt_not_one_word_inline.txt", Encoding.UTF8);
+            Action act = () => fileReader.Read();
+
+            act.Should().Throw<Exception>()
+                .WithMessage("The file must contain no more than one word per line!");
+        }
+
+        [UseReporter(typeof(DiffReporter))]
+        [Test]
+        public void FileReader_Read_ShouldReadTxtWithEmptyLinesCorrect()
+        {
+            var fileReader = new FileReader(@$"{PARENT_DIR_PATH}/txt_with_emptyline.txt", Encoding.UTF8);
+
+            Approvals.Verify(CreateWordsOutput(fileReader.Read()));
+        }
+
+        [UseReporter(typeof(DiffReporter))]
+        [Test]
+        public void FileReader_Read_ShouldReadTxtWithWhiteSpacesCorrect()
+        {
+            var fileReader = new FileReader($@"{PARENT_DIR_PATH}/txt_trim_spaces.txt", Encoding.UTF8);
+
+            Approvals.Verify(CreateWordsOutput(fileReader.Read()));
+        }
+
+        public string CreateWordsOutput(IEnumerable<string> words)
+        {
+            var sb = new StringBuilder();
+            bool isFirstWord = true;
+
+            foreach (var word in words)
+            {
+                sb.AppendLine($"word: '{word}'");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
