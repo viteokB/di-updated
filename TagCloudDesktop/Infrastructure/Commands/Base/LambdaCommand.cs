cd362a1c80f3830392118bb1f025aa6a4b7774b1
Braidenbikher Victor <viktor.brai@yandex.ru>
2024-12-25 10:02:48 +0500

    Sketched the interface

---
+++ TagCloudDesktop/Infrastructure/Commands/Base/LambdaCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagCloudDesktop.Infrastructure.Commands.Base
+{
+    public class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecure = null)
+        {
+            _Execute = Execute ?? throw new ArgumentException(nameof(Execute));
+            _CanExecute = CanExecure;
+        }
+        public override bool CanExecute(object? parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object? parameter) => _Execute(parameter);
+    }
+}
